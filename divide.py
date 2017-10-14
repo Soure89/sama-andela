b6b4db4ea5ddc2d0fdff9435430f3b43e7cf6c70
Saveria Oure <soure@SamaDc.corp>
2017-10-14 11:12:54 +0300

    print

--- divide.py
+++ divide.py
@@ -1,11 +1,15 @@
 
 
-def add_two_numbers(a,b):
-	return (a+b)
-add_two_numbers(5,6)
+def add_two_numbers(a,b,c):
+	return (a+b+c)
+# add_two_numbers(5,6)
 
-
+ 
 def divide_two_numbers(a,b):
 	return (a/b)
 
 divide_two_numbers(5,2)	
+
+answer = add_two_numbers(10, 15, 20)
+print (answer)
+
